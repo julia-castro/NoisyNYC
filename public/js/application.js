e641bee5f17dedf728b6d6f56f7b51523c728c94
julia-castro <julia.castro@outlook.com>
2015-09-19 13:11:19 -0400

    set initial view

--- public/js/application.js
+++ public/js/application.js
@@ -14,6 +14,6 @@ $(document).ready(function() {
 
   L.mapbox.accessToken = 'pk.eyJ1Ijoiam1jYXN0cm8iLCJhIjoiY2llcjl2N2x6MDFneHNtbHpubjR3enhlZCJ9.3A7IZVloogRznOfadjSoGg';
   var map = L.mapbox.map('map', 'jmcastro.cier6sgeg01h9silzqx44aeaw')
-    .setView([40, -74.50], 9);
+    .setView([40.7903, -73.9597], 13);
 
 });
